bc70d6a3eabca8c340687afed3f08685f1e4da80
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-22 13:22:59 +0530

    added hamborgir

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      Home page UwU
+      <nav id="header">
+        <div className="burger">
+          <i style={{fontSize: "26px", paddingTop: "10px"}} className="fa-solid fa-bars"></i>
+        </div>
+      </nav>
+      <script src="https://kit.fontawesome.com/d3e43fefda.js" crossorigin="anonymous"></script>
     </div>
   )
 }
