54ecab336ca32724e7d957b1e55abc532206622e
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-22 17:55:00 +0530

    added batches/animations

--- components/Batch.jsx
+++ components/Batch.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import styles from '../styles/Home.module.css'
 import Image from "next/image";
 
-export default function Batch(props){
+export default function Batch(props) {
     return (
-        <div className="batch-container">
-            <Image className="rounded-2xl" src={props.image} width={350} height={200} />
+        <div style={{ paddingTop: "10px", border: "solid 2px #0070f3;" }} className="batch-container ml-10 mt-5 top-0 right-0 inline">
+            <Image className={styles.batchImage} src={props.image} width={350} height={160} />
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div>
       {/* navbar code */}
-      <nav style={{boxShadow: "0 2px 2px -2px rgba(0,0,0,.2)"}} id="header">
+      <nav style={{ boxShadow: "0 2px 2px -2px rgba(0,0,0,.2)" }} id="header">
         <div className='nav-items relative pb-4'>
           <ul className='items'>
-            <li style={{ marginLeft: "20px" }} className='navbar-item inline-block'><i style={{ fontSize: "40px" }} className="fa-solid fa-bars"></i></li>
-            <div style={{marginLeft: "38%"}} className="inline-block">
-              <div style={{marginLeft: "0"}} className="inline-block">
+            <li style={{ marginLeft: "20px" }} className='navbar-item inline-block pt-2'><i style={{ fontSize: "40px" }} className="fa-solid fa-bars"></i></li>
+            <div style={{ marginLeft: "38%" }} className="inline-block">
+              <div style={{ marginLeft: "0" }} className="inline-block">
                 <li style={{ marginLeft: "20px" }} className='navbar-item text-3xl font-bold inline-block whitespace-normal text-center'>PHYSICS WALLAH</li>
                 <li style={{ marginLeft: "20px" }} className='absolute top-0 right-0 navbar-item inline-block whitespace-normal mr-5'><Image src="/../public/pw_dark.png" width={60} height={60} /></li>
               </div>
@@ -20,15 +20,26 @@ export default function Home() {
         </div>
       </nav>
       {/* batches and stuff */}
-      <div style={{backgroundColor: "#e8e8e8"}} className='batches-page'>
+      <div style={{ backgroundColor: "#e8e8e8", height: "100vh" }} className='batches-page'>
         <div className="options">
-          
+
         </div>
         <hr />
-        <Batch image="/../public/batch.jpg" />
-        <Batch image="/../public/batch1.jpg" />
-        <Batch image="/../public/batch2.jpg" />
-        <Batch image="/../public/batch3.jpg" />
+        <div style={{paddingLeft: "10%"}} className='batches relative px-20 mt-10'>
+          <Batch image="/../public/batch.jpg" />
+          <Batch image="/../public/batch1.jpg" />
+          <Batch image="/../public/batch2.jpg" />
+          <br />
+          <br />
+          <Batch image="/../public/batch3.jpg" />
+          <Batch image="/../public/batch2.jpg" />
+          <Batch image="/../public/batch1.jpg" />
+          <br />
+          <br />
+          <Batch image="/../public/batch1.jpg" />
+          <Batch image="/../public/batch2.jpg" />
+          <Batch image="/../public/batch3.jpg" />
+        </div>
       </div>
 
       <script src="https://kit.fontawesome.com/d3e43fefda.js" crossorigin="anonymous"></script>
