35c134635c2daa55fc2cca8f613207ef0739e13a
FireHead90544 <rudranshjoshi1806@gmail.com>
2022-06-22 19:33:43 +0530

    Added an easter egg after log in :)

--- pages/batches.js
+++ pages/batches.js
@@ -3,6 +3,9 @@ export default function Home() {
   return (
     <div>
       <h1>Thou art loggeth in ^_^</h1>
+      <button onclick="window.location.href='https://www.youtube.com/watch?v=LLFhKaqnWwk';">
+        You are supposed to click here for a cookie &gt;_&lt;
+      </button>
     </div>
-  )
+  );
 }
