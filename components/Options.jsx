cbd70f6dcce69187f11c5294e2f518fe90a3d17c
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-23 16:10:39 +0530

    added neumorphic elements.

--- components/Options.jsx
+++ components/Options.jsx
@@ -5,27 +5,20 @@ export default function Options() {
     return (
         <div className="flex justify-end">
             {/* search bar */}
-            <div className="ml-5">
-                <input className={`${styles.searchBar} drop-shadow-2xl px-2 mx-1  placeholder:text-gray-400`} style={{ borderRadius: "10px", width: "20rem", height: "2.7rem" }} type="text" placeholder="Search batches here"></input>
-                <button className={styles.searchBtn}>Search</button>
+            <div class={`${styles.search} mr-4`}>
+                <input type="text" class={styles.search__input} spellCheck="false" placeholder="Search for batches" />
+                <div className={`${styles.btn} ${styles.btn__primary}`}><p>Search</p></div>
             </div>
             {/* filter button */}
-            <div className="filter inline ml-5 drop-shadow-2xl">
-                <button className={`${styles.filterBtn}`}><span className={styles.filterText}>Filters</span></button>
-            </div>
+            <div class={`${styles.btn} ${styles.btn__secondary}`}><p>Filters</p></div>
             {/* toggle */}
             <div title="toggle dark mode" className={`${styles.toggleButton} ml-5 mt-1 mx-1 toggle-button inline drop-shadow-3xl`}>
-                <label class="label">
-                    <div className={styles.toggle}>
-                        <input className={styles.toggleState} type="checkbox" name="check" value="check" />
-                        <div className={styles.indicator}></div>
+                <div className={`${styles.switch}`}>
+                    <div className={styles.switch__1}>
+                        <input id="switch-1" type="checkbox" />
+                        <label for="switch-1"></label>
                     </div>
-                    <div className={styles.labelText}></div>
-                </label>
-                {/* <label className={styles.switch}>
-                    <input type="checkbox" />
-                    <span className={`${styles.slider} ${styles.round} float-right`}></span>
-                </label> */}
+                </div>
             </div>
         </div>
     )
