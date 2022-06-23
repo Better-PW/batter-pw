5d7a883ac43833a93d0710c642003d7160de29fd
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-23 13:59:13 +0530

    added neumorphic toggle.

--- components/Options.jsx
+++ components/Options.jsx
@@ -6,7 +6,7 @@ export default function Options() {
         <div className="flex justify-end">
             {/* search bar */}
             <div className="ml-5">
-                <input className={`${styles.searchBar} drop-shadow-2xl px-2 mx-1 placeholder:italic  placeholder:text-gray-400`} style={{ borderRadius: "10px", width: "20rem", height: "2.7rem" }} type="text" placeholder="Search batches here"></input>
+                <input className={`${styles.searchBar} drop-shadow-2xl px-2 mx-1  placeholder:text-gray-400`} style={{ borderRadius: "10px", width: "20rem", height: "2.7rem" }} type="text" placeholder="Search batches here"></input>
                 <button className={styles.searchBtn}>Search</button>
             </div>
             {/* filter button */}
@@ -15,10 +15,17 @@ export default function Options() {
             </div>
             {/* toggle */}
             <div title="toggle dark mode" className={`${styles.toggleButton} ml-5 mt-1 mx-1 toggle-button inline drop-shadow-3xl`}>
-                <label className={styles.switch}>
+                <label class="label">
+                    <div className={styles.toggle}>
+                        <input className={styles.toggleState} type="checkbox" name="check" value="check" />
+                        <div className={styles.indicator}></div>
+                    </div>
+                    <div className={styles.labelText}></div>
+                </label>
+                {/* <label className={styles.switch}>
                     <input type="checkbox" />
                     <span className={`${styles.slider} ${styles.round} float-right`}></span>
-                </label>
+                </label> */}
             </div>
         </div>
     )
