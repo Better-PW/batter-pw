bf341424dc195c25de4c25c2f63ed9ebab5627ac
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-23 12:51:51 +0530

    added search button and styling.

--- components/Options.jsx
+++ components/Options.jsx
@@ -6,14 +6,15 @@ export default function Options() {
         <div className="flex justify-end">
             {/* search bar */}
             <div className="ml-5">
-                <input className={`${styles.searchBar} px-2 mx-1 placeholder:italic  placeholder:text-gray-400`} style={{ borderRadius: "5px", width: "20rem", height: "2.7rem" }} type="text" placeholder="Search batches here"></input>
+                <input className={`${styles.searchBar} drop-shadow-2xl px-2 mx-1 placeholder:italic  placeholder:text-gray-400`} style={{ borderRadius: "10px", width: "20rem", height: "2.7rem" }} type="text" placeholder="Search batches here"></input>
+                <button className={styles.searchBtn}>Search</button>
             </div>
             {/* filter button */}
-            <div className="filter inline ml-5">
+            <div className="filter inline ml-5 drop-shadow-2xl">
                 <button className={`${styles.filterBtn}`}><span className={styles.filterText}>Filters</span></button>
             </div>
             {/* toggle */}
-            <div className={`${styles.toggleButton} ml-5 mx-1 toggle-button inline`}>
+            <div title="toggle dark mode" className={`${styles.toggleButton} ml-5 mt-1 mx-1 toggle-button inline drop-shadow-3xl`}>
                 <label className={styles.switch}>
                     <input type="checkbox" />
                     <span className={`${styles.slider} ${styles.round} float-right`}></span>
