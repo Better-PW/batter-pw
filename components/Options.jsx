ef19537698241f3e8f417c966da8ecc00d0e8ef2
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-23 12:05:14 +0530

    media query no work any idea?

--- components/Options.jsx
+++ components/Options.jsx
@@ -6,14 +6,14 @@ export default function Options() {
         <div className="flex justify-end">
             {/* search bar */}
             <div className="ml-5">
-                <input className='px-2 mx-1 placeholder:italic  placeholder:text-gray-400' style={{ borderRadius: "5px", width: "20rem", height: "2.7rem" }} type="text" placeholder="Search batches here"></input>
+                <input className={`${styles.searchBar} px-2 mx-1 placeholder:italic  placeholder:text-gray-400`} style={{ borderRadius: "5px", width: "20rem", height: "2.7rem" }} type="text" placeholder="Search batches here"></input>
             </div>
             {/* filter button */}
             <div className="filter inline ml-5">
                 <button className={`${styles.filterBtn}`}><span className={styles.filterText}>Filters</span></button>
             </div>
             {/* toggle */}
-            <div className='ml-5 mx-1 toggle-button inline'>
+            <div className={`${styles.toggleButton} ml-5 mx-1 toggle-button inline`}>
                 <label className={styles.switch}>
                     <input type="checkbox" />
                     <span className={`${styles.slider} ${styles.round} float-right`}></span>
