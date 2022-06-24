d970d0bfeef6a063d67296bb48ca2bdf6485da34
ankushKun <ankush4singh@gmail.com>
2022-06-24 18:24:22 +0530

    fuck titans alakhroll

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
             <div className='p-2 mr-8 ml-8'>
                 <ul className='flex flex-row text-center justify-between items-center font-poppins'>
                     <li className='text-3xl font-bold w-full tracking-widest'>PHYSICS WALLAH</li>
-                    <li className='justify-self-end'><a href="/alakhroll"><Image src="/media/pw_dark.png" width={35} height={40} /></a></li>
+                    <li className='justify-self-end'><Image src="/media/pw_dark.png" width={35} height={40} /></li>
                 </ul>
             </div>
         </nav>
