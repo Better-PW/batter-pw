c95283e5a144e205c740d0750593495ecd9c6ae0
Arnab Roy <102012992+arnabhere@users.noreply.github.com>
2022-06-24 12:20:00 +0530

    fixed alignment

--- components/Options.jsx
+++ components/Options.jsx
@@ -5,16 +5,16 @@ export default function Options() {
     return (
         <div className="flex justify-end">
             {/* search bar */}
-            <div className="ml-5">
+            <div className="pt-3 mt-2.5">
                 <input className={`${styles.searchBar} drop-shadow-2xl px-2 mx-1  placeholder:text-gray-400`} style={{ borderRadius: "10px", width: "20rem", height: "2.7rem" }} type="text" placeholder="Search batches here"></input>
-                <button className={styles.searchBtn}>Search</button>
+                <button className={styles.searchBtn}>SEARCH</button>
             </div>
             {/* filter button */}
-            <div className="filter inline ml-5 drop-shadow-2xl">
-                <button className={`${styles.filterBtn}`}><span className={styles.filterText}>Filters</span></button>
+            <div className="inline drop-shadow-2xl ml-2 mt-3 pt-3">
+                <button className={`${styles.filterBtn}`}><span className={styles.filterText}>FILTERS</span></button>
             </div>
             {/* toggle */}
-            <div title="toggle dark mode" className={`${styles.toggleButton} ml-5 mt-1 mx-1 toggle-button inline drop-shadow-3xl`}>
+            <div title="toggle dark mode" className={`${styles.toggleButton} mt-2 ml-2 mx-8 pt-5 toggle-button inline drop-shadow-3xl`}>
                 <label>
                     <div className={styles.toggle}>
                         <input className={styles.toggleState} type="checkbox" name="check" value="check" />
