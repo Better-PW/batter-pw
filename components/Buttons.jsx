b8b899105cb7511d5a0ba900cbaaf4e650cf3eb6
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-25 19:04:52 +0530

    buttons fixed.

--- components/Buttons.jsx
+++ components/Buttons.jsx
@@ -3,30 +3,44 @@ import Router, { useRouter } from "next/router";
 import styles from "../styles/Home.module.css"
 
 export default function Buttons() {
-    function myBatches(){
+    function myBatches() {
         Router.push("/batches")
     }
-    function allBatches(){
+    function allBatches() {
         Router.push("/all-batches")
     }
+
     const router = useRouter()
-    useEffect(()=>{
-        if (router.pathname === "/batches"){
-            document.querySelector(".myBatches").classList.add(`${styles.clicked}`);
-        } else {
-            document.querySelector(".myBatches").classList.remove(`${styles.clicked}`);
-            document.querySelector(".allBatches").classList.add(`${styles.clicked}`)
+    useEffect(() => {
+        const theme = localStorage.getItem("theme");
+        if (router.pathname === "/batches") {
+            if (theme === "dark") {
+                document.querySelector(".myBatches").classList.remove(`${styles.clicked}`);
+                document.querySelector(".spanTxt1").classList.remove(`${styles.buttonText}`)
+                document.querySelector(".myBatches").classList.add(`${styles.darkClicked}`)
+            } else {
+                document.querySelector(".myBatches").classList.add(`${styles.clicked}`);
+            }
+        } else if (router.pathname === "/all-batches") {
+            if (theme === "dark") {
+                document.querySelector(".allBatches").classList.remove(`${styles.clicked}`);
+                document.querySelector(".spanTxt2 ").classList.remove(`${styles.buttonText}`)
+                document.querySelector(".allBatches").classList.add(`${styles.darkClicked}`)
+            } else {
+                document.querySelector(".allBatches").classList.add(`${styles.clicked}`)
+            }
+
         }
     })
     return (
         <div className="pt-5 pl-10">
-            <div style={{height: "3rem", width: "14rem"}} className="buttonContainer flex bg-gray-400 rounded-lg">
+            <div style={{ height: "3rem", width: "14rem" }} className="buttonContainer flex bg-gray-400 rounded-lg">
                 <div className="container">
                     <div className="btnAll inline">
-                        <button onClick={allBatches} style={{height: "3rem", width: "7rem"}} className="allBatches focus:bg-gray-300 rounded-lg duration-500 dark:focus:bg-zinc-600"><span className={styles.buttonText}>ALL BATCHES</span></button>
+                        <button onClick={myBatches} style={{ height: "3rem", width: "7rem" }} className="myBatches focus:bg-gray-300 rounded-lg duration-500 dark:focus:bg-slate-800"><span className={`spanTxt2 ${styles.buttonText}`}>MY BATCHES</span></button>
                     </div>
                     <div className="btnMy inline">
-                        <button onClick={myBatches} style={{height: "3rem", width: "7rem"}} className="myBatches focus:bg-gray-300 rounded-lg duration-500 dark:focus:bg-zinc-600 text-white"><span className={styles.buttonText}>MY BATCHES</span></button>
+                        <button onClick={allBatches} style={{ height: "3rem", width: "7rem" }} className="allBatches focus:bg-gray-300 rounded-lg duration-500 dark:focus:bg-slate-800"><span className={`spanTxt1 ${styles.buttonText}`}>ALL BATCHES</span></button>
                     </div>
                 </div>
             </div>
