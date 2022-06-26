4ef50d2df68f7151700eb9b4382c84199da6042d
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-26 19:23:31 +0530

    added meta tags ready to deployyyyyyyyy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import loggedIn from "../components/LoggedIn";
 import Spinner from "../components/spinner";
 import { useRouter } from "next/router";
+import Head from 'next/head'
 
 export default function Home() {
   const router = useRouter();
@@ -18,6 +19,10 @@ export default function Home() {
   }
   return (
     <div>
+      <Head>
+        <meta name="description" content="Better Version of PW Website made by students for students ;)" />
+        <meta name="keywords" content="better pw, pw, better physicswallah, physicswallah, pw nextjs, pankaj pw, titan pw, ankush pw, arnab pw, ag pw, rudransh pw"></meta>
+      </Head>
       <Spinner />
     </div>
   );
