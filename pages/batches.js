ad111159a422457efaa99c11331973bb44b56212
Pankaj Kukreja <pirated.kukreja@gmail.com>
2022-06-26 18:00:18 +0530

    Fixed a minor bug with the batch card component where the app would crash if thumbnail data for some batches was not available

--- pages/batches.js
+++ pages/batches.js
@@ -21,8 +21,6 @@ export default function Batches() {
     }
   }
 
-
-
   useEffect(() => {
     if (!gotBatches) { getBatches(); }
   }, [gotBatches])
@@ -30,7 +28,7 @@ export default function Batches() {
   useEffect(() => {
     if (myBatches.length > 0) {
       let c = [];
-      myBatches.forEach((item) => { c.push(<Batch key={item.slug} batchJson={item} />) });
+      myBatches.forEach((item) => {c.push(<Batch key={item.slug} batchJson={item} />)});
       setBatchCards(c);
     }
   }, [myBatches]);
