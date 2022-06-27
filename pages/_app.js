da6cb0e442850114dfd790cb01b5e64d61bf0ef9
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-27 12:05:43 +0530

    console log go brrrr

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
+  for (var i=0; i<10;i++){
+    console.log('%c JOIN OUR DEV TEAM', 'font-size:40px; background:url(https://media.discordapp.net/attachments/988729878444642377/990863369156329563/unknown.png) no-repeat;');
+    console.log("https://discord.gg/fWJ9n3nwQu")
+  }
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Component {...pageProps} />
