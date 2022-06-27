e6869212c25c7226e047de039c3b21679d61a659
Titan-Codes <vedansh1sharma2005@gmail.com>
2022-06-27 14:41:07 +0530

    Developer page profile card.

---
+++ components/AvatarsCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+
+export default function AvatarCard(props) {
+    return (
+        <div className="ml-5 card bg-slate-100 flex flex-col items-center justify-center p-4 shadow-lg rounded-2xl w-64 dark:bg-gray-800">
+            <div className="main grid place-items-center h-half">
+                {/* profile picture */}
+                <div className="profile mx-auto rounded-full py-2 w-16 ">
+                    <Image className="rounded-full" src={props.src} width={200} height={200} />
+                </div>
+                {/* Name */}
+                <div className="name text-gray-800 text-2xl font-medium mt-4 dark:text-white">
+                    <p>{props.name}</p>
+                </div>
+                {/* work done */}
+                <div className="work text-gray-700 mt-4 dark:text-gray-500">
+                    <p>{props.work}</p>
+                </div>
+                {/* github link */}
+                <div className="w-full mt-8">
+                    <button className="bg-blue-500 py-2 px-4 hover:bg-blue-600 text-white w-full font-semibold rounded-lg shadow-lg">
+                        Github
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
--- pages/developers.js
+++ pages/developers.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Image from "next/image";
+import AvatarCard from "../components/AvatarsCard";
 
 export default function Developers(){
     return (
         <>
           <Navbar />
-          <h1 className="text-3xl text-center">No developers?</h1>
-          <div className="flex items-center justify-center"><Image className="" src="/media/beaches.png" width={300} height={300} /></div>
-          
+          <h1 className="text-3xl text-center mt-10">Better PW Developers</h1>
+          <p className="text-center mt-5 text-gray-700 dark:text-gray-500">Better PW is an un-offical project by made by students for students :D</p>
+          {/* <div className="flex items-center justify-center"><Image className="" src="/media/beaches.png" width={300} height={300} /></div> */}
+          <AvatarCard name="Pankaj" work="DevOps / PM" src="/media/pankaj.png" />
+          <AvatarCard name="Ankush" work="DevOps" src="/media/ankush.png" />
+          <AvatarCard name="Vedansh" work="DevOps" src="/media/vedansh.png" />
+          <AvatarCard name="Rudransh" work="DevOps" src="/media/rudra.png" />
+          <AvatarCard name="Arnab" work="Ui/UX Designer" src="/media/arnab.gif" />
+          <AvatarCard name="Arunava" work="Ui/UX Designer" src="/media/ag.png" />
         </>
     )
 }
